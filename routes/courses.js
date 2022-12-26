8c0f39a46fef0464deb55566f3750178cf5358d4
pgf96 <pgfxx1@gmail.com>
2022-12-26 14:45:35 -0500

    add routes for courses

---
+++ routes/courses.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+const coursesCtrl = require('../controllers/courses');
+// const isLoggedIn = require('../config/auth')
+
+// GET /movies - show all movied
+router.get('/', coursesCtrl.index)
+router.get('/users', coursesCtrl.user)
+router.get('/new', coursesCtrl.new)
+router.get('/new/:id/content', coursesCtrl.newContent)
+router.get('/:id', coursesCtrl.show)
+
+router.post('/', coursesCtrl.create)
+router.post('/new/:id/content', coursesCtrl.createContent)
+
+module.exports = router
