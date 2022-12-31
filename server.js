68634f15c742fa06e21c9df836ffabef71834aed
pgf96 <pgfxx1@gmail.com>
2022-12-31 12:29:03 -0500

    add methodoverride to server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session')
 var passport = require('passport');
+var methodOverride = require('method-override')
 
 // database/env
 require('dotenv').config();
@@ -27,6 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cookieParser());
+app.use(methodOverride('_method'))
 
 app.use(session({
   secret: process.env.SECRET,
