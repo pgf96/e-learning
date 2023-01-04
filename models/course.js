1ba84a42f1a6c6de2059ec8f99553e9fc7bb33d4
pgf96 <pgfxx1@gmail.com>
2023-01-03 23:16:59 -0500

    update course model to include description

--- models/course.js
+++ models/course.js
@@ -1,32 +1,39 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
-const contentSchema = new Schema({
-    sectionTitle: {type: String},
-    description: {type: String},
-    Information: {type: String},
-    file: {type: String},
-}, {
-    timestamps: true
-})
+const contentSchema = new Schema(
+  {
+    sectionTitle: { type: String },
+    description: { type: String },
+    body: { type: String },
+    file: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+)
 
-const courseSchema = new Schema({
-    title:{
-        type: String,
+const courseSchema = new Schema(
+  {
+    title: {
+      type: String,
     },
     user: {
-        type: Schema.Types.ObjectId, ref: 'User'
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    description: {
+      type: String,
     },
     outcomes: {
-        type: [String],
-        default: undefined,
+      type: [String],
+      default: undefined,
     },
-    contents: [contentSchema]
-}, {
-    timestamps: true
-})
+    contents: [contentSchema],
+  },
+  {
+    timestamps: true,
+  }
+)
 
-
-
-
-module.exports = mongoose.model('Course', courseSchema)
+module.exports = mongoose.model("Course", courseSchema)
