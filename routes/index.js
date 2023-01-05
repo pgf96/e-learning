9fad0719ca1b6e4b0264bae45370b2643694658f
pgf96 <pgfxx1@gmail.com>
2023-01-05 16:34:41 -0500

    add google auth index

--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,38 @@
-var express = require('express');
-var router = express.Router();
-var passport = require('passport')
+const express = require("express")
+const router = express.Router()
+const passport = require("passport")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.redirect('/courses')
-});
+router.get("/", function (req, res, next) {
+  res.redirect("/courses")
+})
 
 // Google OAuth login route
-router.get('/auth/google', passport.authenticate(
-  'google',
-  {
-    scope: ['profile', 'email'],
+router.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
     // Optionally force pick account every time
     // prompt: "select_account"
-  }
-));
+  })
+)
 
 // Google OAuth callback route
-router.get('/oauth2callback', passport.authenticate(
-  'google',
-  {
-    successRedirect : '/courses',
-    failureRedirect : '/courses'
-  }
-));
-// OAuth logout route
-router.get('/logout', function(req, res){
-  req.logout(function(err){
-    if (err) {return next(err)}
-    res.redirect('/courses');
+router.get(
+  "/oauth2callback",
+  passport.authenticate("google", {
+    successRedirect: "/courses",
+    failureRedirect: "/courses",
   })
-});
+)
+// OAuth logout route
+router.get("/logout", function (req, res) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err)
+    }
+    res.redirect("/courses")
+  })
+})
 
-
-
-
-module.exports = router;
+module.exports = router
