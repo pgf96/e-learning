70530f02b33ce335928386b11d6bd058eca34081
pgf96 <pgfxx1@gmail.com>
2023-01-05 14:36:01 -0500

    add edit and update route

--- routes/courses.js
+++ routes/courses.js
@@ -1,19 +1,22 @@
-var express = require('express');
-var router = express.Router();
-const coursesCtrl = require('../controllers/courses');
+var express = require("express")
+var router = express.Router()
+const coursesCtrl = require("../controllers/courses")
 // const isLoggedIn = require('../config/auth')
 
 // GET /movies - show all movied
-router.get('/', coursesCtrl.index)
-router.get('/users', coursesCtrl.user)
-router.get('/new', coursesCtrl.new)
-router.get('/new/:id/content', coursesCtrl.newContent)
+router.get("/", coursesCtrl.index)
+router.get("/users", coursesCtrl.user)
+router.get("/new", coursesCtrl.new)
+router.get("/new/:id/content", coursesCtrl.newContent)
 
-router.get('/:id', coursesCtrl.show)
+router.get("/:id", coursesCtrl.show)
 
-router.delete('/:id', coursesCtrl.delete)
+router.put("/:id/edit", coursesCtrl.edit)
+// router.put('/:id', coursesCtrl.update)
 
-router.post('/', coursesCtrl.create)
-router.post('/new/:id/content', coursesCtrl.createContent)
+router.delete("/:id", coursesCtrl.delete)
+
+router.post("/", coursesCtrl.create)
+router.post("/new/:id/content", coursesCtrl.createContent)
 
 module.exports = router
