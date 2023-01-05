6c3dff8a6c7a59c13c70f5d34225b52b64fb8d00
pgf96 <pgfxx1@gmail.com>
2023-01-05 16:33:03 -0500

    delete accordion scroll feature

--- public/javascripts/accordionScroll.js
+++
@@ -1,9 +0,0 @@
-let accordionEl = document.querySelector(".accordion-item")
-
-accordionEl.addEventListener("click", function () {
-  this.scrollIntoView({
-    alignToTop: true,
-    behavior: "smooth",
-    block: "start",
-  })
-})
